0872f9f2ab3b80b19a3abff774938feaaca35e77
ilario-moreni <ilario2001.mor@gmail.com>
2023-01-31 14:50:11 +0100

    milestone 1

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+
+$selected_num = $_GET['num'];
+$characters_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+function generatePassword($selected_num, $characters_list)
+{
+    if (!$selected_num) {
+        return null;
+    }
+    return substr(str_shuffle($characters_list), 0, $selected_num);
+}
+
 ?>
 
 
@@ -10,11 +22,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <title>php-strong-password-generator</title>
 </head>
 
 <body>
-
+    <div class='container py-5'>
+        <div class='row'>
+            <div class='col-auto'>
+                <form action="index.php" method='GET'>
+                    <select name='num' class="form-select" aria-label="Default select example">
+                        <option value='null'>Open this select menu</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
+                    </select>
+                    <div class="col-12 my-3">
+                        <button type="submit" class="btn btn-primary">select number</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col'>
+                Password: '
+                <?php
+                echo generatePassword($selected_num, $characters_list);
+                ?>'
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
