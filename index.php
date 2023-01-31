0172ab310c2299f02653eafdb0e41c5b7981b45b
ilario-moreni <ilario2001.mor@gmail.com>
2023-01-31 14:53:09 +0100

    milestone 2

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
 
+include 'functions.php'
 
-$selected_num = $_GET['num'];
-$characters_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-
-function generatePassword($selected_num, $characters_list)
-{
-    if (!$selected_num) {
-        return null;
-    }
-    return substr(str_shuffle($characters_list), 0, $selected_num);
-}
 
 ?>
 
